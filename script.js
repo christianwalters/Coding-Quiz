09b7f1f2155546bd1c7dae893e9c1e65cada0eba
christianjwalters2002@gmail.com <christianjwalters2002@gmail.com>
2022-10-11 20:32:17 -0500

    completed html and css

---
+++ script.js
@@ -0,0 +1,8 @@
+//array for questions, 5 questions
+//array for possible answers
+//array for correct answers
+//variable for score
+//timer function 60 seconds
+//when button is clicked, start quiz
+//if a question is wrong, subtract from score and subtract from timer 10 seconds
+//if timer equals 0 OR quiz is over, show score to player and let them type in their initials, save this and put it on high score board
